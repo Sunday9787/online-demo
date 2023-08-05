097ae0ea74cb5586a88176292ea6690b72cbc32b
代士翔 <809537981@qq.com>
2023-08-06 03:40:38 +0800

    feat: 3d 地图 添加飞行动画

--- src/views/three/utils/angle.js
+++ src/views/three/utils/angle.js
@@ -5,3 +5,11 @@
 export function angelToRad(deg) {
   return (deg * Math.PI) / 180
 }
+
+/**
+ * 弧度转角度
+ * @param {number} deg
+ */
+export function radToAngle(rad) {
+  return (Math.PI / 180) * rad
+}
